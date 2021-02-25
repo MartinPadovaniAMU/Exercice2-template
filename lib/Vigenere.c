fe4cd9d9e6286e3afbd11f21555bb606b53c7dd3
“Padovani <martin.padovani@etu.univ-amu.fr>
2021-02-25 19:07:29 +0100

    Passe les tests du déchiffrage de Vigenere

--- lib/Vigenere.c
+++ lib/Vigenere.c
@@ -1,5 +1,7 @@
 #include "Vigenere.h"
 #include <ctype.h>
+#include <limits.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -35,7 +37,29 @@ char* dechiffre_Vigenere(
     char* chiffre,      // Texte chiffré qui sera modifié.
     char const* cle)
 {
-    return NULL;
+    int i = 0;
+    int j = 0;
+    char c0 = '\0'; // Pour factoriser le code, on effectue le choix du caractère à soustraire en amont
+    while(chiffre[i] != '\0') // Pour chaque caractère
+    {
+        if(islower(chiffre[i]))
+            c0 = 'a';
+        else if(isupper(chiffre[i]))
+            c0 = 'A';
+        else
+            c0 = '\0';
+        if(c0) // Si l'objet est une lettre
+        {
+            chiffre[i] = chiffre[i] - c0; // On convertit le caractère en nombre ordinal (position dans l'alphabet)
+            chiffre[i] = chiffre[i] - (cle[j++ % strlen(cle)] - 'a'); // On chiffre (il faut également convertir la clé et en faire un tableau circulaire)
+            chiffre[i] = 25 - chiffre[i]; // Inversion de l'ordre des lettres pour faire fonctionner le modulo
+            chiffre[i] = chiffre[i] % 26; // Si la lettre est à la fin de l'alphabet, on revient au début
+            chiffre[i] = 25 - chiffre[i]; // Inversion de l'ordre des lettres pour faire fonctionner le modulo
+            chiffre[i] = chiffre[i] + c0; // On reconvertit en lettre
+        }
+        i++;
+    }
+    return chiffre;
 }
 
 void chiffre_Vigenere_flux_texte(
