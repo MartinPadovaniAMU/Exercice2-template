733b7d0f109a78cbfebf43ae4fb7f901596e197d
“Padovani <martin.padovani@etu.univ-amu.fr>
2021-02-25 13:27:55 +0100

    Passe les tests du chiffrage de Cesar sans majuscules ni ponctuation

--- lib/Cesar.c
+++ lib/Cesar.c
@@ -1,12 +1,27 @@
 #include "Cesar.h"
-
+#include <ctype.h>
 #include <stddef.h>
 
 char* chiffre_Cesar(
     char* clair,        // Texte en clair qui sera modifié.
     char const cle)
 {
-    return NULL;
+    int i = 0;
+    char c0; // Pour factoriser le code, on effectue le choix du caractère à soustraire en amont
+    while(clair[i] != '\0') // Pour chaque caractère
+    {
+        if(islower(clair[i]))
+            c0 = 'a';
+        else if(isupper(clair[i]))
+            c0 = 'A';
+        
+        clair[i] = clair[i] - c0; // On convertit le caractère en nombre ordinal (position dans l'alphabet)
+        clair[i] = clair[i] + (cle - c0); // On chiffre (il faut également convertir la clé)
+        clair[i] = clair[i] % 26; // Si la lettre est à la fin de l'alphabet, on revient au début
+        clair[i] = clair[i] + c0; // On reconvertit en lettre
+        i++;
+    }
+    return clair;
 }
 
 char* dechiffre_Cesar(
