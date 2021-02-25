5e07519c27382d5ef87fd4b896a9cf9031e28d58
“Padovani <martin.padovani@etu.univ-amu.fr>
2021-02-25 11:26:40 +0100

    Passe les tests du déchiffrage ROT13

--- lib/ROT13.c
+++ lib/ROT13.c
@@ -30,5 +30,28 @@ char* chiffre_ROT13(
 char* dechiffre_ROT13(
     char* chiffre)  // Texte chiffré qui sera modifié.
 {
-    return NULL;
+    int i = 0;
+    while(chiffre[i] != '\0') // Pour chaque caractère
+    {
+        if(islower(chiffre[i]))
+        {
+            chiffre[i] = (chiffre[i] - 'a'); // On convertit le caractère en nombre ordinal (position dans l'alphabet)
+            chiffre[i] = chiffre[i] - 13; // On déchiffre (- 13)
+            chiffre[i] = 25 - chiffre[i]; // Inversion de l'ordre des lettres pour faire fonctionner le modulo
+            chiffre[i] = chiffre[i] % 26; // Si la lettre est à la fin de l'alphabet, on revient au début
+            chiffre[i] = 25 - chiffre[i]; // Inversion de l'ordre des lettres pour faire fonctionner le modulo
+            chiffre[i] = chiffre[i] + 'a'; // On reconvertit en lettre
+        }
+        else if(isupper(chiffre[i]))
+        {
+            chiffre[i] = (chiffre[i] - 'A'); // On convertit le caractère en nombre ordinal (position dans l'alphabet)
+            chiffre[i] = chiffre[i] - 13; // On déchiffre (- 13)
+            chiffre[i] = 25 - chiffre[i]; // Inversion de l'ordre des lettres pour faire fonctionner le modulo
+            chiffre[i] = chiffre[i] % 26; // Si la lettre est à la fin de l'alphabet, on revient au début
+            chiffre[i] = 25 - chiffre[i]; // Inversion de l'ordre des lettres pour faire fonctionner le modulo
+            chiffre[i] = chiffre[i] + 'A'; // On reconvertit en lettre
+        }
+        i++;
+    }
+    return chiffre;
 }
